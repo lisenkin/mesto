4bfcb89ae1dd880785e9e12563aba9d95554db79
Masha Zavalina <rennka@ya.ru>
2021-03-17 23:17:29 +0100

    feat: js form and sumbit + html code format

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-console.log('ok');
+
 
 // по клику открыть попап (добавить класс)
 
@@ -27,3 +27,36 @@ openPopupBtn.addEventListener('click' ,function(){
 
  closePopupBtn.addEventListener('click', function() {closePopup(); });
  popupOverlay.addEventListener('click', function() {closePopup(); });
+
+//получим с профиля данные
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+
+//для отладки
+console.log(profileName.value);
+
+ // Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-status');
+
+//для отладки
+console.log(nameInput.value);
+console.log(jobInput.value);
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+     
+    // Получите значение полей jobInput и nameInput из свойства value
+    profileName.textContent = nameInput.value;
+    profileStatus.textContent = jobInput.value;
+
+    
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
